8dc836a1f40001aa9bf79b77115341a761ba17a0
Candace Wilson <cwilson@Candaces-MacBook-Pro-13.local>
2020-12-15 10:01:58 -0500

    save

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -84,7 +84,7 @@
           <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri( '/images/apples.jpg' ); ?>);">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">An Apple a Day</h2>
+                <h2 class="headline headline--medium t-center">An Apple a Day </h2>
                 <p class="t-center">Our dentistry program recommends eating apples.</p>
                 <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
               </div>
